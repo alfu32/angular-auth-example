7c1a286f9fb8764ab8db89bbc41d35aea52e7c23
alfu32 <ioan.alferaru@gmail.com>
2020-02-06 16:28:59 +0100

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
-import { AngularFireAuth } from '@angular/fire/auth';
-import * as firebase  from '@firebase/app';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <div *ngIf="auth.user | async as user; else showLogin">
-      <h1>Hello {{ user.displayName }}!</h1>
-      <button (click)="logout()">Logout</button>
-    </div>
-    <ng-template #showLogin>
-      <p>Please login.</p>
-      <button (click)="login()">Login with Google</button>
-    </ng-template>
-  `,
-  // templateUrl: './app.component.html',
-  // styleUrls: [ './app.component.css' ]
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements OnInit  {
-  constructor(public auth: AngularFireAuth) {
+  loginStream: ReplaySubject<string> = new ReplaySubject<string>();
+  login: string;
+  name = 'Angular';
+  ngOnInit() {
+    this.loginStream.subscribe(
+      v => {
+        console.log(v);
+        this.login = v
+      }
+    )
   }
-  login() {
-    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-  }
-  logout() {
-    this.auth.signOut();
-  }
-  ngOnInit() {}
-  // loginStream: ReplaySubject<string> = new ReplaySubject<string>();
-  // login: string;
-  // name = 'Angular';
-  // ngOnInit() {
-  //   this.loginStream.subscribe(
-  //     v => {
-  //       console.log(v);
-  //       this.login = v
-  //     }
-  //   )
-  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginService } from './login-page/login.service';
-
 const firebaseConfig = {
-    apiKey: "AIzaSyAeG-jUl7yW_MLggch9iMkGEzPgkEx5WKM",
-    authDomain: "angular-s4tzya.firebaseapp.com",
-    databaseURL: "https://angular-s4tzya.firebaseio.com",
-    projectId: "angular-s4tzya",
-    storageBucket: "angular-s4tzya.appspot.com",
-    messagingSenderId: "766308278725",
-    appId: "1:766308278725:web:8fdac918d5909dec192bac",
-    measurementId: "G-3TRK547CT7"
-  };
+  apiKey: "AIzaSyAeG-jUl7yW_MLggch9iMkGEzPgkEx5WKM",
+  authDomain: "angular-s4tzya.firebaseapp.com",
+  databaseURL: "https://angular-s4tzya.firebaseio.com",
+  projectId: "angular-s4tzya",
+  storageBucket: "angular-s4tzya.appspot.com",
+  messagingSenderId: "766308278725",
+  appId: "1:766308278725:web:8fdac918d5909dec192bac",
+  measurementId: "G-3TRK547CT7"
+};
 
 @NgModule({
-  imports:      [
-    BrowserModule,
-    FormsModule,
-    AngularFireAuthModule,
-    AngularFirestoreModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    HttpClientModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent, LoginPageComponent ],
   bootstrap:    [ AppComponent ],
   providers: [LoginService]
